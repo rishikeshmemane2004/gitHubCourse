ec3fdbefe9ddeb02e15b239d493b7c2ff05aa346
RM <rishimemane@gmail.com>
2022-01-26 15:39:24 +0530

    added/updated/deltec files

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -3,5 +3,8 @@ package Pages;
 public class LoginPage {
 	public static void main(String[] args) {
 		System.out.println("Hello LP");
+		String un = "admin";
+		String pwd = "admin";
+		System.out.println("logged in ...");
 	}
 }
